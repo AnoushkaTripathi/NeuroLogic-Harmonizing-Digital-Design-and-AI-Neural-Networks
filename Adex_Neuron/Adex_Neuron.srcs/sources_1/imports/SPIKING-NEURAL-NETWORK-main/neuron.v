8aed4db95b516bf2e4af6ae491a8b29b2c3ce6b3
Anoushka  Tripathi <98522737+AnoushkaTripathi@users.noreply.github.com>
2024-04-09 06:34:28 +0530

    Update neuron.v

--- Adex_Neuron/Adex_Neuron.srcs/sources_1/imports/SPIKING-NEURAL-NETWORK-main/neuron.v
+++ Adex_Neuron/Adex_Neuron.srcs/sources_1/imports/SPIKING-NEURAL-NETWORK-main/neuron.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 29.05.2022 09:17:22
-// Design Name: 
-// Module Name: neuron
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 
